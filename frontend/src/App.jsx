ccfcd86a2a278dcecca4e3b66458c38dbf1ef629
gladwin2802 <ajgladwin2802@gmail.com>
2024-04-20 16:02:53 +0530

    usermodule

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
 
 function App() {
-  return (
-    <div>
-        <h1>Welcome</h1>
-    </div>
-  )
+
+    const { user } = useAuthContext()
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className="pages">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={user ? <Home /> : <Navigate to="/login" />}
+                        />
+                        <Route
+                            path="/login"
+                            element={!user ? <Login /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path="/signup"
+                            element={!user ? <SignUp /> : <Navigate to="/" />}
+                        />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div >
+    );
 }
 
-export default App
+export default App;
